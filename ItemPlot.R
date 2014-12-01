1fcf0d273547cbc8d5d288f65f82d713f7c902b0
Francisco Ospina <francisco.ospina@gmail.com>
2014-12-01 13:35:00 -0500

    Se eliminan lineas innecesatias de ItemPlot.R

--- ItemPlot.R
+++ ItemPlot.R
@@ -19,21 +19,7 @@ plot.item = function(zita = est$zita, item,numboot,alpha ){
   boot[,3] = ifelse(boot[,3] >= 1,1 - 1e-6,boot[,3])
   boot[,3] = ifelse(boot[,3] <= 0,sqrt(2.2e-16),boot[,3])
   boot[,1] = ifelse(boot[,1] <= 0,sqrt(2.2e-16),boot[,1])
-  print("+++++++++++++++++++")
-  print(apply(X = boot,MARGIN = 2,max))
-  print(apply(X = boot,MARGIN = 2,min))
-  print("----------------")
-  print(y[1])
-  #print(boot)
-  #dens = dmvnorm(x = boot,mean = mean,sigma = inf)
-  #sortdens = sort(dens)
-  #lower <- sortdens[floor(length(dens) * alpha / 2)]
-  #upper <- sortdens[ceiling(length(dens) * (1 - alpha / 2))]
-  #index = sapply(c(lower,upper),FUN = function(x){which(dens == x)})
-  #zitalow = boot[index[1],]
-  #zitaup = boot[index[2],]
-  #ylow = sapply(X = x,FUN = gg,a = zitalow[1],d = zitalow[2],cp = qlogis(zitalow[3]))
-  #yup = sapply(X = x,FUN = gg,a = zitaup[1],d = zitaup[2],cp = qlogis(zitaup[3]))
+ 
   envelop = matrix(0,nrow = length(x),ncol = 2)
   for(i in 1:length(x)){
     trace = apply(X = boot,
